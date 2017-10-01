a794556013b1a527743bf71247180297be60c22e
Marius Orcsik <marius@habarnam.ro>
2017-10-01 15:04:51 +0200

    More integration tests

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -1,9 +1,9 @@
 package tests
 
 import (
-	"testing"
 	"activitypub"
 	"jsonld"
+	"testing"
 )
 
 func TestAcceptSerialization(t *testing.T) {
@@ -11,11 +11,29 @@ func TestAcceptSerialization(t *testing.T) {
 	o.Name = make(activitypub.NaturalLanguageValue, 1)
 	o.Name["en"] = "test"
 
-	ctx := jsonld.Context{URL:"https://www.w3.org/ns/activitystreams"}
+	ctx := jsonld.Context{URL: "https://www.w3.org/ns/activitystreams"}
 
 	bytes, err := jsonld.Marshal(o, &ctx)
 	if err != nil {
 		t.Errorf("Error: %v", err)
 	}
 	t.Logf("%s", bytes)
+}
+
+func TestCreateActivityHTTPSerialization(t *testing.T) {
+	id := activitypub.ObjectId("test_object")
+	o := activitypub.AcceptNew(id)
+	o.Name["en"] = "Accept New"
+
+	baseUri := string(activitypub.ActivityBaseURI)
+	c := jsonld.Context{
+		URL: baseUri + o.Type,
+	}
+
+	out, err := jsonld.Marshal(o, &c)
+	//out, err := jsonld.Marshal(o)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("%s", out)
 }
