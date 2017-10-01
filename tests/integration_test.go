f811f14bd38b124b8fcbc3e20cbc3bb0f8c77e48
Marius Orcsik <marius@habarnam.ro>
2017-10-01 14:44:49 +0200

    Adding a basic integration test

---
+++ tests/integration_test.go
@@ -0,0 +1,21 @@
+package tests
+
+import (
+	"testing"
+	"activitypub"
+	"jsonld"
+)
+
+func Test_AcceptSerialization(t *testing.T) {
+	o := activitypub.AcceptNew("https://localhost/myactivity")
+	o.Name = make(activitypub.NaturalLanguageValue, 1)
+	o.Name["en"] = "test"
+
+	ctx := jsonld.Context{URL:"https://www.w3.org/ns/activitystreams"}
+
+	bytes, err := jsonld.Marshal(o, &ctx)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	t.Logf("%s", bytes)
+}
