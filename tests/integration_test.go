c68df3052b21165cde116bf84aed42494706fddc
Marius Orcsik <marius@habarnam.ro>
2018-03-26 23:15:26 +0200

    Add multilang name in test

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -12,6 +12,7 @@ func TestAcceptSerialization(t *testing.T) {
 	obj := activitypub.AcceptNew("https://localhost/myactivity", nil)
 	obj.Name = make(activitypub.NaturalLanguageValue, 1)
 	obj.Name["en"] = "test"
+	obj.Name["fr"] = "teste"
 
 	ctx := jsonld.Context{URL: "https://www.w3.org/ns/activitystreams"}
 
@@ -27,6 +28,9 @@ func TestAcceptSerialization(t *testing.T) {
 	}
 	if !strings.Contains(string(data), string(obj.Name["en"])) {
 		t.Errorf("Could not find name %#v in output %s", string(obj.Name["en"]), data)
+	}
+	if !strings.Contains(string(data), string(obj.Name["fr"])) {
+		t.Errorf("Could not find name %#v in output %s", string(obj.Name["fr"]), data)
 	}
 	if !strings.Contains(string(data), string(obj.Type)) {
 		t.Errorf("Could not find activity type %#v in output %s", obj.Type, data)
