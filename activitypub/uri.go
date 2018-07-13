66767f675afe3b39e6b58a3be5a51c806809124c
Marius Orcsik <marius.orcsik@rocket-internet.de>
2018-07-13 10:17:03 +0200

    Made URI and IRI conform to the LinkOrUri interface
    Added a convenience String method

--- activitypub/uri.go
+++ activitypub/uri.go
@@ -7,3 +7,17 @@ type (
 	// URI is a Uniform Resource Identifier (URI) RFC3986
 	URI string
 )
+
+func (u URI) String() string {
+	return string(u)
+}
+func (i IRI) String() string {
+	return string(i)
+}
+
+func (u URI) GetLink() URI {
+	return u
+}
+func (i IRI) GetLink() URI {
+	return URI(i)
+}
