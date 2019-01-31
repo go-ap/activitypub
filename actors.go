29c67b42e5ce1f2bee3570fab9741c50419a9961
Marius Orcsik <marius.orcsik@rocket-internet.de>
2019-01-31 13:24:34 +0100

    Making Actor types aliases of Object

--- actors.go
+++ actors.go
@@ -17,37 +17,6 @@ var validActorTypes = [...]ActivityVocabularyType{
 	ServiceType,
 }
 
-// Endpoints a json object which maps additional (typically server/domain-wide)
-// endpoints which may be useful either for this actor or someone referencing this actor.
-// This mapping may be nested inside the actor document as the value or may be a link to
-// a JSON-LD document with these properties.
-type Endpoints struct {
-	// UploadMedia Upload endpoint URI for this user for binary data.
-	UploadMedia Item `jsonld:"uploadMedia,omitempty"`
-	// OauthAuthorizationEndpoint Endpoint URI so this actor's clients may access remote ActivityStreams objects which require authentication
-	// to access. To use this endpoint, the client posts an x-www-form-urlencoded id parameter with the value being
-	// the id of the requested ActivityStreams object.
-	OauthAuthorizationEndpoint Item `jsonld:"oauthAuthorizationEndpoint,omitempty"`
-	// OauthTokenEndpoint If OAuth 2.0 bearer tokens [RFC6749] [RFC6750] are being used for authenticating client to server interactions,
-	// this endpoint specifies a URI at which a browser-authenticated user may obtain a new authorization grant.
-	OauthTokenEndpoint Item `jsonld:"oauthTokenEndpoint,omitempty"`
-	// ProvideClientKey  If OAuth 2.0 bearer tokens [RFC6749] [RFC6750] are being used for authenticating client to server interactions,
-	// this endpoint specifies a URI at which a client may acquire an access token.
-	ProvideClientKey Item `jsonld:"provideClientKey,omitempty"`
-	// SignClientKey If Linked Data Signatures and HTTP Signatures are being used for authentication and authorization,
-	// this endpoint specifies a URI at which browser-authenticated users may authorize a client's public
-	// key for client to server interactions.
-	SignClientKey Item `jsonld:"signClientKey,omitempty"`
-	// SharedInbox If Linked Data Signatures and HTTP Signatures are being used for authentication and authorization,
-	// this endpoint specifies a URI at which a client key may be signed by the actor's key for a time window to
-	// act on behalf of the actor in interacting with foreign servers.
-	SharedInbox Item `jsonld:"sharedInbox,omitempty"`
-}
-
-type WillAct interface {
-	GetActor() Actor
-}
-
 // Actor is generally one of the ActivityStreams Actor Types, but they don't have to be.
 // For example, a Profile object might be used as an actor, or a type from an ActivityStreams extension.
 // Actors are retrieved like any other Object in ActivityPub.
