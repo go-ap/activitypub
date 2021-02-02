15fe66e9aa5d91caad4b560d574f1b845ec836d0
mariusor <marius@federated.id>
2021-02-02 14:46:30 +0100

    Add some tests for AddTo method

--- typer_test.go
+++ typer_test.go
@@ -29,10 +29,6 @@ func TestSplit(t *testing.T) {
 	t.Skipf("TODO")
 }
 
-func TestCollectionType_AddTo(t *testing.T) {
-	t.Skipf("TODO")
-}
-
 func TestCollectionType_IRI(t *testing.T) {
 	t.Skipf("TODO")
 }
@@ -92,6 +88,78 @@ func TestIRIf(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := IRIf(tt.args.i, tt.args.t); got != tt.want {
 				t.Errorf("IRIf() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCollectionType_AddTo(t *testing.T) {
+	type args struct {
+		i activitypub.Item
+	}
+	var i activitypub.Item 
+	var o *activitypub.Object
+	tests := []struct {
+		name  string
+		t     CollectionType
+		args  args
+		want  activitypub.IRI
+		want1 bool
+	}{
+		{
+			name:  "simple",
+			t:     "test",
+			args:  args{
+				i: &activitypub.Object{ID: "http://example.com/addTo"},
+			},
+			want:  "http://example.com/addTo/test",
+			want1: false, // this seems to always be false
+		},
+		{
+			name:  "on-nil-item",
+			t:     "test",
+			args:  args{
+				i: i,
+			},
+			want: activitypub.NilIRI,
+			want1: false,
+		},
+		{
+			name:  "on-nil",
+			t:     "test",
+			args:  args{
+				i: nil,
+			},
+			want: activitypub.NilIRI,
+			want1: false,
+		},
+		{
+			name:  "on-nil-object",
+			t:     "test",
+			args:  args{
+				i: o,
+			},
+			want: activitypub.NilIRI,
+			want1: false,
+		},
+		{
+			name:  "on-nil-item",
+			t:     "test",
+			args:  args{
+				i: i,
+			},
+			want: activitypub.NilIRI,
+			want1: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := tt.t.AddTo(tt.args.i)
+			if got != tt.want {
+				t.Errorf("AddTo() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("AddTo() got1 = %v, want %v", got1, tt.want1)
 			}
 		})
 	}
